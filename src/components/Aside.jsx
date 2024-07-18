4c01e1c6231d79d29db5356e7f8f8c9e8fe67c3d
Mirzazada Bunyad <115315853+MirzazadaBunyad@users.noreply.github.com>
2024-07-18 19:00:14 +0400

    update

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,23 +1,37 @@
-import SearchLogo from "../../public/assets/search.png";
-import MoreHoriz from "../../public/assets/more_horizontal.png";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchPatients } from '../ReduxToolkit/ThunkAPI/AsyncThunk';
 import { setSelectedPatient } from '../ReduxToolkit/Features/dataSlice';
+import SearchLogo from "../../public/assets/search.png";
+import PatientItem from './UI/PatientItem/PatientItem';
 
-function Aside() {
+const usePatients = () => {
     const dispatch = useDispatch();
     const { fetchedData, error } = useSelector((state) => state.data.patients);
-    const [selectedIdx, setSelectedIdx] = useState(null);
 
     useEffect(() => {
         dispatch(fetchPatients());
     }, [dispatch]);
 
-    const names = fetchedData ? fetchedData.map(person => person.name) : [];
-    const profilePhotos = fetchedData ? fetchedData.map(person => person.profile_picture) : [];
-    const gender = fetchedData ? fetchedData.map(person => person.gender) : [];
-    const age = fetchedData ? fetchedData.map(person => person.age) : [];
+    useEffect(() => {
+        if (fetchedData && fetchedData.length > 0) {
+            dispatch(setSelectedPatient({ ...fetchedData[0], index: 0 }));
+        }
+    }, [fetchedData, dispatch]);
+
+    return { fetchedData, error };
+};
+
+function Aside() {
+    const { fetchedData, error } = usePatients();
+    const [selectedIdx, setSelectedIdx] = useState(0);
+    const dispatch = useDispatch();
+
+    const handlePatientClick = (index) => {
+        setSelectedIdx(index);
+        const selectedPatient = { ...fetchedData[index], index };
+        dispatch(setSelectedPatient(selectedPatient));
+    };
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -26,16 +40,6 @@ function Aside() {
     if (!fetchedData) {
         return <div>Loading...</div>;
     }
-
-    const handlePatientClick = (index) => {
-        if (selectedIdx === index) {
-            setSelectedIdx(null);
-        } else {
-            setSelectedIdx(index);
-        }
-        const selectedPatient = { ...fetchedData[index], index };
-        dispatch(setSelectedPatient(selectedPatient));
-    };
 
     return (
         <aside className="bg-white mt-[32px] w-[20%] rounded-[16px] h-[1010px] overflow-hidden">
@@ -46,42 +50,17 @@ function Aside() {
                 </button>
             </div>
             <div className="flex flex-col gap-[16px] mt-[20px] max-h-full overflow-y-auto custom-scrollbar">
-                {names.map((name, index) => (
-                    <div
+                {fetchedData.map((person, index) => (
+                    <PatientItem
                         key={index}
-                        className={`patient-item flex justify-between p-[16px] cursor-pointer items-center ${selectedIdx === index ? 'selected' : ''
-                            }`}
-                        onClick={() => handlePatientClick(index)}
-                    >
-                        <div className="flex w-full gap-[12px] items-center">
-                            <div className="w-[48px] h-[48px]">
-                                {profilePhotos[index] ? (
-                                    <img
-                                        className="w-full h-full object-cover"
-                                        src={profilePhotos[index]}
-                                        alt="Profile"
-                                    />
-                                ) : (
-                                    "No Image"
-                                )}
-                            </div>
-                            <div className="flex w-full gap-[12px] justify-between items-center">
-                                <div>
-                                    <h2 className="font-bold text-[14px] text-left">{name}</h2>
-                                    <h4 className="text-[14px] text-[#707070] text-left">
-                                        {gender[index]}, {age[index]}
-                                    </h4>
-                                </div>
-                                <button>
-                                    <img
-                                        src={MoreHoriz}
-                                        className="w-[18px] h-[4px]"
-                                        alt="More options"
-                                    />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                        index={index}
+                        name={person.name}
+                        profilePhoto={person.profile_picture}
+                        gender={person.gender}
+                        age={person.age}
+                        selectedIdx={selectedIdx}
+                        handleClick={handlePatientClick}
+                    />
                 ))}
             </div>
         </aside>
